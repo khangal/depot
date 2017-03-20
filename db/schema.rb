cc52b611d2d4a1ee26f3d6e88cc583d8330f9dc0
Khangal <khangaljargal@yahoo.com>
2017-03-20 18:23:53 +0800

    create order

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170317083000) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.integer  "quantity",   default: 1
+    t.decimal  "price"
     t.integer  "order_id"
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["order_id"], name: "index_line_items_on_order_id"
