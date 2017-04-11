13a3b4e72bfbf22fb32b40288a8608b12b896a30
Khangal <khangaljargal@yahoo.com>
2017-04-11 13:28:19 +0900

    Display dollars in euro on Espanol

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,9 @@ module ApplicationHelper
     end
     content_tag("div", attributes, &block)
   end
+
+  def number_to_price(number)
+    number = number * 0.94 if I18n.locale == :es
+    number_to_currency(number)
+  end
 end
